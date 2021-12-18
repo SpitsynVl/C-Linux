4a46b05f1c8a44d92eb7b378a2c2ecde51d2aa6d
SpitsynVI <vovaspicyn18@gmail.com>
2021-12-18 17:16:39 +0300

    wait for child proccess

---
+++ 13_1.c
@@ -0,0 +1,54 @@
+#define _GNU_SOURCE 
+#include <stdio.h> 
+#include <sys/types.h> 
+#include <unistd.h>
+#include <errno.h>  
+#include <sys/wait.h>
+#include <sys/stat.h> 
+#include <stdlib.h> 
+#include <signal.h>
+#include <string.h>
+
+int main(void) { 
+    pid_t child_id = fork(); 
+    if(child_id < 0) {
+        perror("fork");
+        return 1;
+    }  
+
+    if(child_id == 0) { //работает только дочерний процесс
+        printf("Child process "); 
+        pid_t child = getpid(); 
+        printf("PID = %d\n", child);
+        sleep(60);
+    } else { //работает только родительский процесс
+        int status = 0; 
+        if(waitpid(child_id, &status, WUNTRACED | WCONTINUED) < 0) {
+            perror("waitpid"); 
+            return 2; 
+        }  
+        while(WIFEXITED(status) == 0 && WIFSIGNALED(status) == 0) {//выходит, когда дочерний процесс завершился или получил сигнал
+            if(WIFSTOPPED(status)) {
+                printf("Child with PID = %d was stopped by signal %d (%s)\n", child_id, WSTOPSIG(status), sigdescr_np(WTERMSIG(status)));     
+            } 
+            if(WIFCONTINUED(status)) {
+                printf("Child with PID = %d continue to work\n", child_id);
+            } 
+            if(waitpid(child_id, &status, WUNTRACED | WCONTINUED) < 0) {
+                perror("waitpid"); 
+                return 3;
+            }
+        }
+        if(WIFEXITED(status)) { 
+            printf("Child process with PID = %d exited with code %d\n", child_id, WEXITSTATUS(status));
+        } 
+        else if(WIFSIGNALED(status)) {
+            int child_signal = WTERMSIG(status);
+            printf("Child with PID = %d was killed by signal %d (%s)\n",  child_id, child_signal, sigdescr_np(child_signal));
+            if(WCOREDUMP(status)) {
+                printf("core dump\n");
+            }
+        }
+    }
+    return 0;
+}
