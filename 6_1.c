7fde5b1a4ce464b0eb00363de7e67125c469af6f
SpitsynVI <vovaspicyn18@gmail.com>
2022-02-08 18:59:09 +0300

    done 6_1

--- 6_1.c
+++ 6_1.c
@@ -1,84 +1,87 @@
 #include <stdio.h>
-#include <sys/types.h>
+#include <string.h>
 #include <sys/stat.h>
-#include <unistd.h>  
-#include <dirent.h> 
-#include <string.h> 
-#include <fcntl.h>
-  
-char mode_letter(unsigned mode) { 
+#include <sys/types.h>
+#include <dirent.h>
+#include <errno.h>
 
-    switch (mode & S_IFMT) {
-           case S_IFBLK:  return 'b';
-           case S_IFCHR:  return 'c';
-           case S_IFDIR:  return 'd';
-           case S_IFIFO: return 'f';
-           case S_IFLNK:  return 'l';
-           case S_IFREG:  return 'r';
-           case S_IFSOCK: return 's';
-           default:       return '?';
-    } 
+char dtype_char(unsigned d_type) {
+    switch(d_type) {
+        case DT_BLK: return 'b';
+        case DT_CHR: return 'c';
+        case DT_DIR: return 'd';
+        case DT_FIFO: return 'p';
+        case DT_LNK: return 'l';
+        case DT_REG: return '-';
+        case DT_SOCK: return 's';
+    }
+    return '?';
 }
 
-
-
-char dtype_letter(unsigned dtype) { 
-
-    switch(dtype) {
-        case DT_BLK : return 'b'; break;    
-
-        case DT_CHR : return 'c'; break;
-
-        case DT_DIR : return 'd'; break;   
-
-        case DT_FIFO : return 'f'; break; 
-
-        case DT_LNK : return 'l'; break;
-
-        case DT_REG : return 'r'; break;    
-
-        case DT_SOCK : return 's'; break;    
-
-        case DT_UNKNOWN : return '?'; break;
-
-        default : return '-';
+char mode_char(mode_t st_mode) {
+    switch (st_mode & S_IFMT) {
+        case S_IFBLK: return 'b'; 
+        case S_IFCHR: return 'c';
+        case S_IFDIR: return 'd'; 
+        case S_IFIFO: return 'p'; 
+        case S_IFLNK: return 'l'; 
+        case S_IFREG: return '-'; 
+        case S_IFSOCK: return 's';	
     }
-
-} 
-
-void FileInformation(const char* dirpath) {
-    DIR* dir_fd = opendir(dirpath);  
-    if(dir_fd == NULL) {
-        perror("open dir failed");
-        return;
-    }  
-    //считываем файлы из данного каталога  
-    struct dirent* entry; 
-    while((entry = readdir(dir_fd)) != NULL) {
-        struct stat sb; 
-        char type = dtype_letter(entry->d_type); 
-        if(type == '?') {
-            struct stat sb; 
-            if(lstat(entry->d_name, &sb) == 0) {
-                type = mode_letter(sb.st_mode);
-            } 
-        }   
-        printf("%c %s\n", type, entry->d_name);
-    }
-    closedir(dir_fd); 
+    return '?';
 }
 
+int main(void) {
+    int status = 0;
 
-int main(int argc, char** argv)  {    
+    /* убрал эту проверку, так как компилятор ругается, что не используем argv[]
+    if(argc > 1) {
+        perror("Too many argumets, I need only one\n");
+        status = 1;
+    }
+    */
 
-    if(argc == 1) {
-        FileInformation("."); 
-    } else { 
-        for(int i = 1; i < argc; i++) {
-            printf("Current directory: %s\n", argv[i]);  
-            FileInformation(argv[i]);
-        } 
+    DIR* cur_dir;
+    cur_dir = opendir(".");
+    if(cur_dir == NULL) {
+        perror("Can't open directory\n");
+        status = 1;
     }
 
-    return 0;
+    struct dirent* entry;
+    errno = 0;
+    while(1) {
+        entry = readdir(cur_dir); 
+        if(entry == NULL) {
+            break;
+        }
+
+        char file_type = dtype_char(entry->d_type);
+
+        //в случае, когда readdir не смог распознать тип файла, пользуемся lstat
+        if(file_type == '?') {
+            struct stat sb;
+            if(lstat(entry->d_name, &sb) == -1) {
+                perror("Error in lstat\n");
+                status = 2;
+                continue;
+            }
+            file_type = mode_char(sb.st_mode);
+        }
+
+        printf("%c %s\n", file_type, entry->d_name);
+    }
+
+    //проверяем, вышли ли мы из while из-за достижения конца директории или из-за ошибки в readdir
+    if(errno != 0) {
+        perror("Error in readdir\n");
+        status = 3;
+    }
+
+    if(closedir(cur_dir) == -1) {
+        perror("Error in closedir\n");
+        status = 4;
+    }
+
+    return status;
 }
